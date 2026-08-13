4f752ea4273d9b640c18adf68dde2196f6c21e20
Danish Ansari <danisha222002@gmail.com>
2026-08-13 17:11:19 +0530

    Add configuration fields for JWT and admin credentials in settings

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,13 @@ from pydantic import Field
 
 class Settings(BaseSettings):
     OPENAI_API_KEY: str = Field(..., description="OpenAI API Key for accessing OpenAI services.")
-
+    DATABASE_URL: str = Field(..., description="Database connection URL for the application.")
+    JWT_SECRET_KEY: str = Field(..., description="Secret key for generating JWT tokens.")
+    JWT_ALGORITHM: str = Field(..., description="Algorithm for generating JWT tokens.")
+    TOKEN_EXPIRY: str = Field(...,description="Token Expiry time for JWT tokens.")
+    ADMIN_NAME: str = Field(...,description="Admin Username For seed")
+    ADMIN_PASSWORD: str = Field(...,description="Admin Password for Seed")
+    
     model_config=SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
