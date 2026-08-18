17481a0dd5d1a4a719e3c91bb962aecfff3ad1fd
Danish Ansari <danisha222002@gmail.com>
2026-08-18 17:42:47 +0530

    feat: implement memory slice with vector support and add required dependencies

--- app/core/database.py
+++ app/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from app.core.config import settings
 from sqlalchemy.orm import sessionmaker,declarative_base
 
-engine = create_engine(settings.DATABASE_URL,echo=True)
+engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
